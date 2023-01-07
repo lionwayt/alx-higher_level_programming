3d8aed4b31e809e4e66178dc87d10bc20765c597
root <hiwotmolalign2@gmail.com>
2023-01-07 07:59:32 +0000

    1. Square with size

---
+++ 1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """Define a Square class"""
+
+    def __init__(self, size):
+        """Defines and initializes the square.
+        size (int): the length of one side of the square.
+        """
+        self.__size = 
