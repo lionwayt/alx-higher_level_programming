8db586c8554c5c78d564800080230feb205bd290
root <hiwotmolalign2@gmail.com>
2023-01-11 16:51:23 +0000

    8. Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+A module for BaseGeometry class.
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A rectangle class."""
+
+    def __init__(self, width, height):
+        """Initialize the rectangle class."""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pr
